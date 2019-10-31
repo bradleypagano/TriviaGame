3222375ebc03f9d4ecaca4f712b0f491984872ff
Bradley Pagano <bpagano5243@gmail.com>
2019-10-30 20:33:59 -0400

    all HTML is complete.  Just have to fix the answer check function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,52 @@
 $(document).ready(function () {
     $(".questionForm").hide();
-    let timer= 50;
+    $("#submitButtonDiv").hide();
+    $(".answers").hide();
+    let timer= 5;
     let gameTime;
+    function asnwerCheck(){
+        let correct = 0;
+        let incorrect = 0;
+        const answers = $('input:checked');
+        for(i = 0; i < answers.length; i++){
+            if (answers[i].value == "right"){
+                correct++;
+            } else{
+                incorrect++;
+            };
+        };
+        $("#corect").text("Right: " + correct);
+        $("#incorrect").text("Wrong: " + incorrect);
+    };
     function countDown(){
         timer--;
         $("#emptyDiv").html("<h5>"+"Seconds Remaining: "+ timer + "</h5>");
         if(timer === 0){
             $("#emptyDiv").empty();
-            $("#emptyDiv").html("<h1>YOU WIN!!!!</h1>");
+            $(".questionForm").empty();
+            $("#submitButtonDiv").empty();
+            asnwerCheck();
+            $(".answers").show();
             clearInterval(gameTime);
             console.log("hey there");
         };
     };
 
-    $("button").on("click", function () {
+    $(".initiate").on("click", function () {
         $("#emptyDiv").empty();
         $("#emptyDiv").append("<h5>"+"Seconds Remaining: "+ timer + "</h5>");
         gameTime = setInterval(countDown,1000);
         $(".questionForm").show();
+        $("#submitButtonDiv").show();
         
+    });
+    $(".done").on("click", function () {
+        $("#emptyDiv").empty();
+        $(".questionForm").empty();
+        $("#submitButtonDiv").empty();
+        asnwerCheck();
+        $(".answers").show();
+        clearInterval(gameTime);
     });
 
 
